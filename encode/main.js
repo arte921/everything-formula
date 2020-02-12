d199bb9ee4031d0a3f763bff27bf054df8cbdfce
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-12 20:49:58 +0100

    s

--- encode/main.js
+++ encode/main.js
@@ -1,2 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var mcbwidth = window.innerWidth;
+var mcbheight = window.innerHeight;
+
+
+if(mcbwidth<mcbheight){
+
+}
