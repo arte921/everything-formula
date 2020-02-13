672b84539bb9e5a2073c76d10b01ddf2c859fa09
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-13 08:42:05 +0100

    y

--- encode/main.js
+++ encode/main.js
@@ -1,9 +1,46 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var mcbwidth = window.innerWidth;
-var mcbheight = window.innerHeight;
+var xw = 106;
+var yw = 17;
 
+var zo = new Array(xw*yw+1).fill(0);
+console.log(zo);
 
-if(mcbwidth<mcbheight){
-
+if(window.innerWidth/xw<window.innerHeight/yw){
+  size=Math.floor(window.innerWidth/xw);
+}else{
+  size=Math.floor(window.innerHeight/yw);
 }
+
+var mcbwidth = xw*size+1;
+var mcbheight = yw*size+1;
+canvas.width=mcbwidth;
+canvas.height=mcbheight;
+
+for(let h=0;h<=xw;h++){
+  ctx.fillRect(h*size,0,1,yw*size);
+}
+
+for(let v=0;v<=yw;v++){
+  ctx.fillRect(0,v*size,xw*size,1);
+}
+
+canvas.addEventListener('click',function(event){
+  let x = Math.floor(event.offsetX/mcbwidth*xw);
+  let y = Math.floor(event.offsetY/mcbheight*yw);
+  let drawx = x*size;
+  let drawy = y*size;
+
+  let index = x*yw+(yw-1)-y;
+  console.log(index);
+  if(zo[index]!=1){
+    zo[index] = 1;
+    ctx.fillRect(drawx,drawy,size,size);
+  }else{
+    zo[index] = 0;
+    ctx.clearRect(drawx+1,drawy+1,size-1,size-1);
+  }
+});
+
+var bin = zo.join('');
+var base10 = parseInt(bin,2);
